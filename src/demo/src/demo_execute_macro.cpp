536ca4397c62723ad748e8a7f21729d2d105ff15
admin <admin@Barebone.ch>
2026-07-24 14:59:42 +0000

    FIXED

--- src/demo/src/demo_execute_macro.cpp
+++ src/demo/src/demo_execute_macro.cpp
@@ -35,40 +35,59 @@ public:
   }
 
   void send_goal()
-  {
-    using namespace std::placeholders;
+{
+  using namespace std::placeholders;
 
-    this->timer_->cancel();
+  this->timer_->cancel();
 
-    if (!this->client_ptr_->wait_for_action_server()) {
-      RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
-      rclcpp::shutdown();
-    }
-
-    auto goal_msg = Action::Goal();
-    goal_msg.command = "executeMacro Macro4";
-    goal_msg.identifier = std::vector<std::string>{"Completed macro Macro4"};
-
-
-    RCLCPP_INFO(this->get_logger(), "Sending goal");
-
-    auto send_goal_options = rclcpp_action::Client<Action>::SendGoalOptions();
-    send_goal_options.goal_response_callback =
-      std::bind(&ActionClient::goal_response_callback, this, _1);
-    send_goal_options.feedback_callback =
-      std::bind(&ActionClient::feedback_callback, this, _1, _2);
-    send_goal_options.result_callback =
-      std::bind(&ActionClient::result_callback, this, _1);
-    this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+  if (!this->client_ptr_->wait_for_action_server()) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Action server not available after waiting");
+    rclcpp::shutdown();
+    return;
   }
+
+  auto goal_msg = Action::Goal();
+
+  goal_msg.command = "executeMacro Macro4";
+  goal_msg.identifier = {"Completed macro Macro4"};
+
+  RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+  auto send_goal_options =
+    rclcpp_action::Client<Action>::SendGoalOptions();
+
+  send_goal_options.goal_response_callback =
+    std::bind(
+      &ActionClient::goal_response_callback,
+      this,
+      _1);
+
+  send_goal_options.feedback_callback =
+    std::bind(
+      &ActionClient::feedback_callback,
+      this,
+      _1,
+      _2);
+
+  send_goal_options.result_callback =
+    std::bind(
+      &ActionClient::result_callback,
+      this,
+      _1);
+
+  this->client_ptr_->async_send_goal(
+    goal_msg,
+    send_goal_options);
+}
 
 private:
   rclcpp_action::Client<Action>::SharedPtr client_ptr_;
   rclcpp::TimerBase::SharedPtr timer_;
 
-  void goal_response_callback(std::shared_future<GoalHandleAction::SharedPtr> future)
+  void goal_response_callback(GoalHandleAction::SharedPtr goal_handle)
   {
-    auto goal_handle = future.get();
     if (!goal_handle) {
       RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
     } else {
@@ -79,26 +98,32 @@ private:
     void feedback_callback(GoalHandleAction::SharedPtr,
         const std::shared_ptr<const Action::Feedback> feedback)
     {
-        RCLCPP_INFO(this->get_logger(), feedback->feed_msg);
+        RCLCPP_INFO(
+        this->get_logger(),
+        "%s",
+        feedback->feed_msg.c_str());
     }
 
-  void result_callback(const GoalHandleAction::WrappedResult & result)
+  void result_callback(
+    const GoalHandleAction::WrappedResult & result)
   {
     switch (result.code) {
       case rclcpp_action::ResultCode::SUCCEEDED:
+        RCLCPP_INFO(this->get_logger(), "Goal succeeded");
         break;
+
       case rclcpp_action::ResultCode::ABORTED:
-        RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
-        return;
+        RCLCPP_ERROR(this->get_logger(), "Goal aborted");
+        break;
+
       case rclcpp_action::ResultCode::CANCELED:
-        RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
-        return;
+        RCLCPP_WARN(this->get_logger(), "Goal canceled");
+        break;
+
       default:
-        RCLCPP_ERROR(this->get_logger(), "Unknown result code");
-        return;
+        RCLCPP_ERROR(this->get_logger(), "Unknown result");
+        break;
     }
-    RCLCPP_INFO(this->get_logger(), result.result->res_msg);
-    rclcpp::shutdown();
   }
 };  // class ActionClient
 
